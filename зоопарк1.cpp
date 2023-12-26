420941b9884b69d043563f6f0718f98ca3490ffa
mvbulihtova <145568037+mvbulihtova@users.noreply.github.com>
2023-12-26 23:50:16 +0300

    Add files via upload

---
+++ зоопарк1.cpp
@@ -0,0 +1,118 @@
+﻿#include <iostream>
+#include <string>
+
+// Base class "Animal"
+class Animal {
+public:
+    std::string name;
+
+    // Add animal to the zoo
+    virtual void AddToZoo() {
+        std::cout << "Added animal: " << name << std::endl;
+    }
+
+    // Remove animal from the zoo
+    virtual void RemoveFromZoo() {
+        std::cout << "Removed animal: " << name << std::endl;
+    }
+
+    // Eat
+    virtual void Eat() {
+        std::cout << name << " is eating" << std::endl;
+    }
+
+    // Swim
+    virtual void Swim() {
+        std::cout << name << " is swimming" << std::endl;
+    }
+
+    // Make sound
+    virtual void MakeSound() {
+        std::cout << name << " makes sounds" << std::endl;
+    }
+
+    // Pet
+    virtual void Pet() {
+        std::cout << "Petted animal: " << name << std::endl;
+    }
+};
+
+// Class "Lion"
+class Lion : public Animal {
+public:
+    int maneLength;
+    bool isKing;
+
+    // Roar
+    void Roar() {
+        std::cout << name << " roars" << std::endl;
+    }
+
+    // Overridden method - eat
+    void Eat() override {
+        std::cout << name << " is a carnivorous animal, hunts and eats meat" << std::endl;
+    }
+};
+
+// Class "Penguin"
+class Penguin : public Animal {
+public:
+    int swimDepth;
+    bool isEmperor;
+
+    // Dance
+    void Dance() {
+        std::cout << name << " dances" << std::endl;
+    }
+
+    // Overridden method - swim
+    void Swim() override {
+        std::cout << name << " swims, using its wings as flippers" << std::endl;
+    }
+};
+
+// Class "Crocodile"
+class Crocodile : public Animal {
+public:
+    int numberOfTeeth;
+    bool isDangerous;
+
+    // Hide underwater
+    void HideUnderwater() {
+        std::cout << name << " hides underwater" << std::endl;
+    }
+
+    // Overridden method - make sound
+    void MakeSound() override {
+        std::cout << name << " makes a characteristic roar" << std::endl;
+    }
+};
+
+int main() {
+    Lion lion;
+    lion.name = "Lion";
+    lion.maneLength = 20;
+    lion.isKing = true;
+
+    Penguin penguin;
+    penguin.name = "Penguin";
+    penguin.swimDepth = 100;
+    penguin.isEmperor = true;
+
+    Crocodile crocodile;
+    crocodile.name = "Crocodile";
+    crocodile.numberOfTeeth = 80;
+    crocodile.isDangerous = true;
+
+    lion.AddToZoo();
+    lion.Roar();
+    lion.Eat();
+
+    penguin.AddToZoo();
+    penguin.Dance();
+
+    crocodile.AddToZoo();
+    crocodile.HideUnderwater();
+
+    return 0;
+}
---
+++ калькулятор.cpp
@@ -0,0 +1,158 @@
+﻿#include <iostream>
+#include <cmath>
+#include <cstdlib>
+
+using namespace std;
+
+double sum(double var1, double var2);
+
+double sub(double var1, double var2);
+
+double mul(double var1, double var2);
+
+double div(double var1, double var2);
+
+unsigned long long fact(int n);
+
+void mat(int matrix1[3][3], int matrix2[3][3]);
+
+
+int main() {
+    while (true) {
+        int choice, var3;
+        double var1, var2;
+        char operation;
+        int matrix1[3][3];
+        int matrix2[3][3];
+
+        cout << "Which type of operation you want to use:" << "\n" << "1 - Sum of matrix" << "\n" << "2 - Factorial"
+            << "\n" << "3 - Common operations" << "\n" << "4 - Exit" << "\n";
+        cin
+            >> choice;
+
+        switch (choice) {
+        case 1:
+            mat(matrix1, matrix2);
+            break;
+
+        case 2:
+            cout << "Enter number to find it factorial:" << "\n";
+            cin >> var3;
+            cout << "Result: " << fact(var3) << endl;
+            break;
+
+
+        case 3:
+            cout << "Enter math expression:" << "\n";
+
+            cin >> var1 >> operation >> var2;
+            switch (operation) {
+            case '+':
+                cout << "Result: " << sum(var1, var2) << endl;
+                break;
+
+            case '*':
+                cout << "Result: " << mul(var1, var2) << endl;
+                break;
+
+            case '-':
+                cout << "Result: " << sub(var1, var2) << endl;
+                break;
+
+            case '/':
+                cout << "Result: " << div(var1, var2) << endl;
+                break;
+
+            case '^':
+                cout << "Result: " << pow(var1, var2) << endl;
+                break;
+
+
+            default:
+                return 0;
+            }
+            break;
+
+        case 4:
+            cout << "Exiting the program";
+            exit(EXIT_SUCCESS);
+
+
+        default:
+            return 0;
+        }
+    }
+}
+
+
+double sum(double var1, double var2) {
+    return var1 + var2;
+}
+
+double sub(double var1, double var2) {
+    return var1 - var2;
+}
+
+double mul(double var1, double var2) {
+    return var1 * var2;
+}
+
+double div(double var1, double var2) {
+    if (var2 == 0) {
+        cout << "You cant divide by zero";
+    }
+    else {
+        return var1 / var2;
+    }
+
+}
+
+unsigned long long int fact(int n) {
+    unsigned long long int result = 1;
+    for (int i = 1; i <= n; i++) {
+        result *= i;
+    }
+    return result;
+}
+
+void mat(int matrix1[3][3], int matrix2[3][3]) {
+    cout << "Enter matrix1:" << "\n";
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            matrix1[i][j] = rand() % 10;
+        }
+    }
+
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            cout << matrix1[i][j] << "\t";
+        }
+        cout << "\n";
+    }
+    cout << "\n";
+
+    cout << "Enter matrix2:" << "\t";
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            matrix2[i][j] = rand() % 10;
+        }
+    }
+
+    cout << "\n";
+
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            cout << matrix2[i][j] << "\t";
+        }
+        cout << "\n";
+    }
+    cout << "\n";
+    cout << "Sum of matrix:" << "\n";
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            cout << matrix1[i][j] + matrix2[i][j] << "\t";
+        }
+        cout << "\n";
+    }
+
+}
